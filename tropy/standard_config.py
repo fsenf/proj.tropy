37c06d25eaf47a2a1d4f00e3c8fb5b5709f91b5b
Fabian Senf <senf@tropos.de>
2018-09-28 09:43:27 +0000

    update of standard_config, more flexible handling of environmental path variables -> only set if existing ...

--- tropy/standard_config.py
+++ tropy/standard_config.py
@@ -2,14 +2,29 @@
 
 import os
 
-local_home_path  =  os.environ['LHOME']
+try:
+    local_home_path  =  os.environ['LHOME']
+    proj_path = '%s/proj' % local_home_path
+    pics_path = '%s/pics' % local_home_path
+except:
+    pass
 
-global_data_path =  os.environ['GLOBAL_DATA_PATH']
-local_data_path  =  os.environ['LOCAL_DATA_PATH']
-meteosat_archive_path =  os.environ['METEOSAT_ARCHIVE_PATH']
+try:
+    global_data_path =  os.environ['GLOBAL_DATA_PATH']
+    cosmo_path = '%s/cosmo/de' % global_data_path
+except:
+    pass
 
 
-cosmo_path = '%s/cosmo/de' % global_data_path
-proj_path = '%s/proj' % local_home_path
-pics_path = '%s/pics' % local_home_path
+try:
+    local_data_path  =  os.environ['LOCAL_DATA_PATH']
+except:
+    pass
+
+try:
+    meteosat_archive_path =  os.environ['METEOSAT_ARCHIVE_PATH']
+except:
+    pass
+
+
 
