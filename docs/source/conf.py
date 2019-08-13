a0b5f1d84ea4e7de36b332193cab1f9e5e2af931
Fabian Senf <senf@tropos.de>
2019-08-13 13:52:42 +0000

    adjusted docstring import for sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,12 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'numpydoc',
+extensions = ['sphinx.ext.autodoc', 
     'sphinx.ext.githubpages', 'sphinx.ext.napoleon']
+ 
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
