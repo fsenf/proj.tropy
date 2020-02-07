2558db0ccb76c03ff1e2f2c78e9587bb04d420a7
Fabian Senf <senf@tropos.de>
2020-02-07 14:15:31 +0100

    further modification for using extension

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@
 
 # from setuptools import setup
 from tropy._version import __version__
-from numpy.distutils.core import setup, Extension
-
-#f2py -c -m --f90flags='-O3 -fPIC' _f90_bit_conversion_altair bit_conversion.f90
-e1 = Extension( '_f90_bit_conversion', 
-                sources = ['tropy/io_tools/bit_conversion.f90',],
-                extra_f90_compile_args = ['-fPIC'], )
 
 setup_kws = dict(
       name='tropy',
@@ -18,14 +12,21 @@ setup_kws = dict(
       license='GPL',
       packages=['tropy', 'tropy.analysis_tools', 'tropy.io_tools', 'tropy.plotting_tools', 'tropy.l15_msevi'],
       ext_package = 'tropy.io_tools',
-      ext_modules = [e1, ], 
       zip_safe=False)
 
 try:
-    setup( **setup_kws )
-except:
+    from numpy.distutils.core import setup, Extension
+
+    #f2py -c -m --f90flags='-O3 -fPIC' _f90_bit_conversion_altair bit_conversion.f90
+    e1 = Extension( '_f90_bit_conversion', 
+                sources = ['tropy/io_tools/bit_conversion.f90',],
+                extra_f90_compile_args = ['-fPIC'], )
+
+
     print('Install without Fortran Extensions')
-    setup_kws['ext_modules'] = []
+    setup_kws['ext_modules'] = [e1, ]
 
     setup( **setup_kws )
 
+except:
+    setup( **setup_kws )
